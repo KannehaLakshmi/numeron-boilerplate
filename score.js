5eca29e7069f626772e48e56c65b550fa2063549
KannehaLakshmi <pji.kanneha.lakshmi@kalvium.community>
2024-07-17 23:38:19 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,10 @@
 // Iteration 5: Store the player score and display it on the game over screen
+const retryButton = document.getElementById("play-again-button");
+const scoreDisplay = document.getElementById("score-board");
+
+if (retryButton)
+  retryButton.onclick = function () {
+    window.location.href = "./index.html";
+  };
+if (scoreDisplay) scoreDisplay.innerText = localStorage.getItem("score");
+
